4f74e6de34cd5b9c400518ba05eef10c9fc91196
shk33 <m.coronel.seg@gmail.com>
2022-07-10 17:00:06 -0500

    Can instantiate a Journey

--- src/entities/Journey.ts
+++ src/entities/Journey.ts
@@ -1,3 +1,28 @@
-class Journey {}
+import { StationName } from './Station';
+import TransportType from '../enums/TransportType';
+
+class Journey {
+    private startPoint: StationName;
+    private endPoint: StationName;
+    private transportType: TransportType;
+
+    constructor(start: StationName, end: StationName, type: TransportType) {
+        this.startPoint = start;
+        this.endPoint = end;
+        this.transportType = type;
+    }
+
+    getStarPoint() {
+        return this.startPoint;
+    }
+
+    getEndPoint() {
+        return this.endPoint;
+    }
+
+    getTransportType() {
+        return this.transportType;
+    }
+}
 
 export default Journey;
--- test/entities/Journey.spec.ts
+++ test/entities/Journey.spec.ts
@@ -1,8 +1,17 @@
 import Journey from '../../src/entities/Journey';
+import TransportType from '../../src/enums/TransportType';
+import { StationName } from '../../src/entities/Station';
 
 describe('Journey', () => {
-    it('should have inital credit of zero', () => {
-        const card = new Card();
-        expect(card.getCurrentCredit()).toEqual(0);
+    it('should initialize a journey', () => {
+        const start = StationName.EARLS_COURT;
+        const end = StationName.HAMMERSMITH;
+        const type = TransportType.TUBE;
+
+        const journey = new Journey(start, end, type);
+
+        expect(journey.getStarPoint()).toEqual(start);
+        expect(journey.getEndPoint()).toEqual(end);
+        expect(journey.getTransportType()).toEqual(type);
     });
 });
